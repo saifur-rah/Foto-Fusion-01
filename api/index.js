2b9a671ffdf30345ae18650259e06dac8ec0d302
Saifur Rahman <84004205+saifur-rah@users.noreply.github.com>
2023-07-21 23:00:34 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -40,8 +40,8 @@ app.use("/api/users", userRoute);
 
 
 //saif edit
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// const __dirname = path.resolve();
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '/client/dist')));
@@ -52,9 +52,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === "PRODUCTION") {
   // set static folder
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '/client/dist')));
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+    res.sendFile(path.join(__dirname, "/client/dist/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
