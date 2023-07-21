7d00350e16595415bae1adfb53a4edb99b87906f
Saifur Rahman <84004205+saifur-rah@users.noreply.github.com>
2023-07-21 23:23:26 +0530

    Update index.js exp

--- api/index.js
+++ api/index.js
@@ -60,11 +60,22 @@ app.use("/api/users", userRoute);
 
 
 
- if (process.env.NODE_ENV === "PRODUCTION") {
-  // set static folder
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+ // if (process.env.NODE_ENV === "PRODUCTION") {
+ //  // set static folder
+ //  app.use(express.static(path.join(__dirname, '../client/dist')));
+ //  app.get("*", (req, res) =>
+ //    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+ //  );
+
+
+   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+
   );
 } else {
   app.get("/", (req, res) => {
