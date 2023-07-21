e9631dd7fb8976e078357cbdb7d4ea64130cb807
Saifur <saifurrahman8051@gmail.com>
2023-07-22 01:09:29 +0530

    second

--- api/index.js
+++ api/index.js
@@ -60,23 +60,25 @@ app.use("/api/users", userRoute);
 
 
 
- // if (process.env.NODE_ENV === "PRODUCTION") {
- //  // set static folder
- //  app.use(express.static(path.join(__dirname, '../client/dist')));
- //  app.get("*", (req, res) =>
- //    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
- //  );
+ 
 
-__dirname=path.resolve();
-   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// __dirname=path.resolve();
+//    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/client/dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
 
-  );
+//   
+if (process.env.NODE_ENV === "PRODUCTION") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+  
+);
 } else {
   app.get("/", (req, res) => {
     res.send("API is running !");
